68cd5f6d927e53e1755eb7b3a744ef6ea2914429
Jeffrey McAteer <jeffrey.p.mcateer@gmail.com>
2023-07-29 11:42:35 -0400

    better continuation

--- boot-pi-emulator.sh
+++ boot-pi-emulator.sh
@@ -10,6 +10,33 @@ set -e # exit immediately on any errors in script
 source config-vars.sh
 
 DISK_TO_INSTALL_TO="$1"
+if [ -z "$DISK_TO_INSTALL_TO" ] && [ -e "vm-files/partitioned-disk-name.txt" ] ; then
+    DISK_TO_INSTALL_TO="$(cat vm-files/partitioned-disk-name.txt)"
+    if ! [ -z "$DISK_TO_INSTALL_TO" ] ; then
+        if which lshw >/dev/null 2>&1 ; then
+            echo ""
+            MOUNTED_DISK_DEVICES=$(mount | grep '/dev/.*on /' | sed 's/ .*//g' | tr '\n' '|')
+            MOUNTED_DISK_DEVICES=${MOUNTED_DISK_DEVICES::-1} # trim last | so we don't match .* in grep
+            MOUNTED_DISK_DEVICES=$(sed 's/p1|/|/g' <<< "$MOUNTED_DISK_DEVICES")
+            MOUNTED_DISK_DEVICES=$(sed 's/1|/|/g' <<< "$MOUNTED_DISK_DEVICES")
+            
+            lshw -class disk -json 2>/dev/null |\
+                jq -r '.[] | select( .logicalname | contains("/dev/") ) | select( has("size") == true ) | "\(.logicalname) \(.product) built by \(.vendor), \(.size/1000000000 | floor) gigabytes"' |\
+                grep -v -E "$MOUNTED_DISK_DEVICES" |\
+                awk '{ print; print ""; }'
+        else
+            echo ""
+            echo "<Please install 'lshw' for a list of disks, vendors, and sizes>"
+            echo ""
+        fi
+
+        read -p "Continue using previously-selected disk $DISK_TO_INSTALL_TO?" YN
+        if ! grep -q "y" <<< "$YN" ; then
+            echo "Exiting..."
+            exit 1
+        fi
+    fi
+fi
 if [ -z "$DISK_TO_INSTALL_TO" ] ; then
     echo "Please select an un-mounted disk to install to."
     echo "If you do not see your disk below please un-mount it first."
@@ -47,7 +74,16 @@ fi
 # Partition $DISK_TO_INSTALL_TO IF not already partitioned!
 echo ""
 touch "vm-files/partitioned-disk-name.txt"
+
+# Yeah yeah, it's a heuristic.
+BOOT_PARTITION="${DISK_TO_INSTALL_TO}1"
+ROOT_PARTITION="${DISK_TO_INSTALL_TO}2"
+
+( sudo umount "vm-files/disk_root/boot" || true ) 2>/dev/null >/dev/null
+( sudo umount "vm-files/disk_root" || true ) 2>/dev/null >/dev/null
+
 fdisk -l "$DISK_TO_INSTALL_TO"
+
 if ! grep -q "$DISK_TO_INSTALL_TO" "vm-files/partitioned-disk-name.txt" ; then
     read -p "About to partition $DISK_TO_INSTALL_TO, continue? (y/n) " YN
     if ! grep -q "y" <<< "$YN" ; then
@@ -71,9 +107,28 @@ if ! grep -q "$DISK_TO_INSTALL_TO" "vm-files/partitioned-disk-name.txt" ; then
   w # write the partition table
   q # and we're done
 EOF
+    sync
+    echo 'y' | sudo mkfs.vfat "$BOOT_PARTITION"
+    echo 'y' | sudo mkfs.ext4 "$ROOT_PARTITION"
+    sync
     echo "$DISK_TO_INSTALL_TO" > "vm-files/partitioned-disk-name.txt" 
 fi
+sync
 echo "Partitioning complete"
+
+
+( sudo umount "vm-files/disk_root/boot" || true ) 2>/dev/null >/dev/null
+( sudo umount "vm-files/disk_root" || true ) 2>/dev/null >/dev/null
+
+mkdir -p "vm-files/disk_root"
+sudo mount "$ROOT_PARTITION" "vm-files/disk_root"
+sudo mkdir -p "vm-files/disk_root/boot"
+sudo mount "$BOOT_PARTITION" "vm-files/disk_root/boot"
+
+if ! [ -e "vm-files/disk_root/root" ] ; then
+    echo "Extracting vm-files/ArchLinuxARM-rpi-aarch64-latest.tar.gz to vm-files/disk_root"
+    sudo bsdtar -xpf "vm-files/ArchLinuxARM-rpi-aarch64-latest.tar.gz" -C "vm-files/disk_root"
+fi
 
 
 echo ""
