4780d434cfd65d01ebfc967bbd0b7dcf3c1cefa4
Jeffrey McAteer <jeffrey.p.mcateer@gmail.com>
2023-09-23 16:52:52 -0400

    added win xp stuff

---
+++ boot-win-qemu.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+set -e
+
+mkdir -p build
+
+win_xp_qcow2='build/Windows-XP.qcow2'
+win_xp_c_root='build/win_xp_c_root'
+
+if ! [[ -e  "$win_xp_qcow2" ]] ; then
+  if [[ -e '/mnt/scratch/torrents/windows-xp-sp3-qcow2/Windows XP.qcow2' ]] ; then
+    cp '/mnt/scratch/torrents/windows-xp-sp3-qcow2/Windows XP.qcow2' "$win_xp_qcow2"
+  else
+    wget -O "$win_xp_qcow2" 'https://ia801409.us.archive.org/34/items/windows-xp-sp3-qcow2/Windows%20XP.qcow2'
+  fi
+fi
+
+echo "win_xp_qcow2 = $win_xp_qcow2"
+
+# Find the RobotAndRobot.com USB device bus and port; currently no USB chains supported!
+lsusb_line=$(lsusb | grep -i 'RobotAndRobot.com')
+if [[ -z "$lsusb_line" ]] ; then
+  echo "Cannot find a RobotAndRobot.com USB control board, exiting!"
+  exit 1
+fi
+bus_num=$(cut -d' ' -f 2 <<<"$lsusb_line" | sed 's/^0*//')
+device_num=$(cut -d' ' -f 4 <<<"$lsusb_line" | sed 's/^0*//' | sed 's/[^0-9]//g')
+
+echo "Forwarding bus $bus_num device $device_num (parsed from $lsusb_line)"
+
+if mount | grep -qi "$win_xp_c_root" ; then
+  sudo umount "$win_xp_c_root" || true
+  sudo qemu-nbd -d /dev/nbd0 || true
+fi
+
+qemu-system-i386 \
+  -enable-kvm -cpu host -m 712M \
+  -vga std -net nic,model=rtl8139 -net user \
+  -drive file="$win_xp_qcow2" \
+  -usb -device usb-host,hostbus=$bus_num,hostport=$device_num
+
+
+
+
+
+
+
+
+
+
---
+++ mount-win-qemu.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+
+win_xp_qcow2='build/Windows-XP.qcow2'
+win_xp_c_root='build/win_xp_c_root'
+
+sudo modprobe nbd max_part=16
+
+if mount | grep -qi "$win_xp_c_root" ; then
+  sudo umount "$win_xp_c_root" || true
+  sudo qemu-nbd -d /dev/nbd0 || true
+fi
+
+sudo qemu-nbd -c /dev/nbd0 "$win_xp_qcow2"
+mkdir -p "$win_xp_c_root"
+
+sudo fdisk -l /dev/nbd0
+
+sudo mount /dev/nbd0p1 "$win_xp_c_root"
+
+echo ''
+echo "Windows XP C: drive mounted at $win_xp_c_root"
+echo ''
+echo ls -alh "$win_xp_c_root"
+echo ''
+
+
+
